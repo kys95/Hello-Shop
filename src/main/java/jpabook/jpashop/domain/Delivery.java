8fe1c3a8394dfdd48b3c4e2628532eb898f5d5c4
Yunseok Kang <mok02198@gmail.com>
2022-02-10 22:38:07 +0900

    Create Delivery.java

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)    //READY와 COMP사이에 추가가되어도 순서에 밀리는 게 없음
+    private DeliveryStatus status; //READY, COMP
+
+}
