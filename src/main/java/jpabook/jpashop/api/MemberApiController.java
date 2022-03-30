2b0de4a9ff93a8db55432e83cb70059dc0ba1c68
Yunseok Kang <mok02198@gmail.com>
2022-03-30 23:52:36 +0900

    Feat: 회원 조회 api 생성

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,46 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+
+    // 문제점 : Member엔티티 모든 속성을 다 리턴하게 되는데 순수하게 회원목록만 원할경우 원치않는 주문정보 등이 포함될 수 있다.
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+
+    //object type으로 반환하는 것이므로 Result라는 껍데기를 씌워줌
+    @GetMapping("api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        // member 엔티티에서 이름을 꺼내와서 MemberDto로 넣음
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member)
